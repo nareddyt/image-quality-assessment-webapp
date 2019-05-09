c2e544abd92e0f951186534a2395bd1ab7b51fbe
Tejasvi Nareddy <tejunareddy@gmail.com>
2019-05-08 20:12:13 -0400

    Working keras->tfjs model conversion script

--- scripts/keras_to_tfjs.py
+++ scripts/keras_to_tfjs.py
@@ -3,22 +3,20 @@ import argparse
 from handlers.model_builder import Nima
 
 
-# FIXME: This script doesn't work as expected. Use the bash command in keras_to_tfjs.sh
-
-def main(keras_model_directory, web_model_directory):
+def main(keras_model_weights, web_model_directory):
 
     # Build keras model and load weights
     nima = Nima('MobileNet', weights=None)
     nima.build()
-    nima.nima_model.load_weights(keras_model_directory)
+    nima.nima_model.load_weights(keras_model_weights)
 
     # Convert to tfjs model
-    tfjs.converters.save_keras_model(nima, web_model_directory)
+    tfjs.converters.save_keras_model(nima.nima_model, web_model_directory)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-k', '--keras-model-directory', help='TODO', required=True)
+    parser.add_argument('-k', '--keras-model-weights', help='TODO', required=True)
     parser.add_argument('-w', '--web-model-directory', help='TODO', required=True)
 
     args = parser.parse_args()
