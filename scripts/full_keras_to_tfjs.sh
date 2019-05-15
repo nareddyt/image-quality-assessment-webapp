72874e93154d0949e43784b89b0d91448016425e
Tejasvi Nareddy <tejunareddy@gmail.com>
2019-05-14 21:52:12 -0400

    Attempt to convert full keras model directly to tfjs

---
+++ scripts/full_keras_to_tfjs.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./venv/Lib/site-packages/tensorflowjs/converters/converter.py --input_format keras ./keras_model/full_model.h5 ./web_model/
---
+++ scripts/weights_keras_to_tfjs.py
@@ -0,0 +1,27 @@
+import tensorflowjs as tfjs
+import argparse
+from handlers.model_builder import Nima
+
+
+def main(keras_model_weights_in):
+
+    # Build keras model and load weights
+    nima = Nima('MobileNet', weights=None)
+    nima.build()
+    nima.nima_model.load_weights(keras_model_weights_in)
+    print(nima.nima_model.summary())
+
+    # Save as full model + weights file
+    nima.nima_model.save('./keras_model/full_model.h5')
+
+    # Convert to tfjs model
+    tfjs.converters.save_keras_model(nima.nima_model, './web_model/')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-k', '--keras-model-weights-in', help='TODO', required=True)
+
+    args = parser.parse_args()
+
+    main(**args.__dict__)
